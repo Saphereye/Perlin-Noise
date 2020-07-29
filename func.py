96ed3754a72d9c9a69e12bce7eecdc6c26c22bfd
BurningPheonix <59739923+BurningPheonix@users.noreply.github.com>
2020-07-29 10:00:33 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 #Seed is the list which makes the code random
+#rang is the size of world eg.[30,10]
 def fun(x,seed):
     return (seed[0]*(sin((seed[1]*x)+seed[2])))
 
@@ -25,7 +26,7 @@ def highest(list):
 				
 def last(val):
     x=val
-		#6,4,1,-9 are the ouputs of the current seed
+#6,4,1,-9 are the ouputs of the current seed
     for k in range(len(val)):
         for i in range(len(val)-2):
             for j in range(len(val[i])-2):
@@ -56,4 +57,10 @@ def last(val):
                             val[i-1][j]=-9
                         if val[i+1][j]!=1 and val[i][j-1]!=4 and val[i][j-1]!=6:
                             val[i+1][j]=-9
-        
+
+def noise(val):
+	#Gives the random set
+    for i in range(len(val)):
+        for j in range(len(val[i])):
+            val[i][j]=int((fun(randint(0,3),seed)))
+    return val
